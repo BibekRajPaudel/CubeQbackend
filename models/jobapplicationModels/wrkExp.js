d90e4b0d7f1828666c27d2ed39bae5d193058570
Pawan Shrestha <pawanshresthait@gmail.com>
2022-11-07 15:55:48 +0545

    work experience model added

---
+++ models/jobapplicationModels/wrkExp.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const WorkExperienceSchema = mongoose.Schema({
+  companyName: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: [2, 'Name cannot be lower than two characters'],
+  },
+  designation: {
+    type: String,
+    trim: true,
+    maxlength: 200,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+  },
+  currentlyWorking: {
+    type: Boolean,
+    required: true,
+  },
+  cv: {
+    type: String,
+  },
+  coverLetter: {
+    type: String,
+  },
+  additionalDoc: [String],
+});
+
+module.exports = mongoose.model('StudentFeedback', StudentFeedbackSchema);
