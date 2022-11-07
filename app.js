177b69243dee21980d7aef4666899569ce5a843c
Pawan Shrestha <pawanshresthait@gmail.com>
2022-11-07 15:41:34 +0545

    route and error handler middleware integrate

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 
+// Error handler middleware import
+const errorHandler = require('./middleware/errorHandler');
+const routeNotFound = require('./middleware/notFound');
+
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res, next) => {
   res.send('Server started !!!!!!!!!!');
 });
+
+app.use(errorHandler);
+app.use(routeNotFound);
