8c9256800b8a9403eb01e24881925c77ae8d3b1e
Pawan Shrestha <pawanshresthait@gmail.com>
2022-11-14 13:33:56 +0545

    admin route and job applt route added

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const connectDB = require('./db/connect');
 
 // Routers
 const scheduleACallRouter = require('./routers/scheduleACallRouter');
+const jobApplyRouter = require('./routers/jobApplyRouter');
+
+// Admin ROuters
+const aboutUsRouter = require('./routers/AdminRouters/aboutUsRouter');
+const ourTeamRouter = require('./routers/AdminRouters/ourTeamRouter');
+const ourServiceRouter = require('./routers/AdminRouters/ourServiceRouter');
 
 // Error handler middleware import
 const errorHandler = require('./middleware/errorHandler');
 const routeNotFound = require('./middleware/notFound');
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -17,6 +27,32 @@ app.get('/', (req, res, next) => {
 });
 
 app.use('/api/call', scheduleACallRouter);
+app.use('/api/jobapply', jobApplyRouter);
+
+// Admin Routes
+app.use('/api/admin/aboutus', aboutUsRouter);
+app.use('/api/admin/ourteam', ourTeamRouter);
+app.use('/api/admin/ourservice', ourTeamRouter);
+
+app.use(
+  '/public/uploads/jobapplications',
+  express.static(path.join(__dirname, '/public/uploads/jobapplications'))
+);
+app.use(
+  '/public/uploads/aboutusimage',
+  express.static(path.join(__dirname, '/public/uploads/jobapplications'))
+);
 
 app.use(errorHandler);
 app.use(routeNotFound);
+
+const PORT = process.env.PORT || 3000;
+
+(async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, console.log(`Server is listening on Port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+})();
