2c5ab91c8ef68a3110347201a46833e862948772
Pawan Shrestha <pawanshresthait@gmail.com>
2022-11-14 13:30:04 +0545

    job application model update

--- models/jobapplicationModels/jobApplication.js
+++ models/jobapplicationModels/jobApplication.js
@@ -1,39 +1,33 @@
 const mongoose = require('mongoose');
 
-// Schemas
-const EducationSchema = require('./education');
-const WorkExperienceSchema = require('./wrkExp');
-
 const JobApplicationSchema = mongoose.Schema({
-  firstName: {
+  fullName: {
     type: String,
-    required: true,
-    trim: true,
-    minlength: [2, 'Name cannot be lower than two characters'],
-  },
-  lastName: {
-    type: String,
-    trim: true,
-    required: true,
-    minlength: [2, 'Name cannot be lower than two characters'],
   },
   email: {
     type: String,
-    required: true,
     trim: true,
     lowercase: true,
-    required: [true, 'Email address is required'],
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       'Please fill a valid email address',
     ],
   },
-  phone: {
+  phoneNumber: {
     type: String,
-    required: true,
   },
-  education: [EducationSchema],
-  workExperience: [WorkExperienceSchema],
+  cv: {
+    type: String,
+  },
+  coverLetter: {
+    type: String,
+  },
+  links: {
+    type: String,
+  },
+  reasonToJoinCubeQ: {
+    type: String,
+  },
 });
 
 module.exports = mongoose.model('JobApplication', JobApplicationSchema);
