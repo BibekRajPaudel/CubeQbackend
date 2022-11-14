5a627b12bc5c2d25cc6560a5b9d8960f42b73adc
Pawan Shrestha <pawanshresthait@gmail.com>
2022-11-14 12:44:52 +0545

    model updated for admin user

--- models/adminUser.js
+++ models/adminUser.js
@@ -2,6 +2,20 @@ const mongoose = require('mongoose');
 
 const AdminUserSchema = mongoose.Schema({
   name: String,
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    required: [true, 'Email address is required'],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please fill a valid email address',
+    ],
+  },
+  password: {
+    type: String,
+  },
   isAdmin: {
     type: Boolean,
   },
