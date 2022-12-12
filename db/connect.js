d687d96b4d7a7af3dc48eefeb16c9ac96cb45c5d
Cubeq <roshan@cubeqbusiness.com>
2022-12-12 08:52:35 +0000

    connected to the right database

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = uri => {
-  return mongoose.connect("mongodb+srv://root_cubeq:TpZEtmMAnwlj1KDV@cluster0.8rmu1ij.mongodb.net/?retryWrites=true&w=majority", {
+  return mongoose.connect("mongodb+srv://root_cubeq:tncfnLaeaBL3wR29@cluster0.8rmu1ij.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
